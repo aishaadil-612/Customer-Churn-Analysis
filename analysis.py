f5efba6775168f98f19bee6bce7623993671c1dc
aishaadil-612 <aishaadil612@gmail.com>
2026-06-10 22:51:14 +0530

    added

--- analysis.py
+++ analysis.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-#df= pd.DataFrame("customer_churn_dataset-testing-master.csv")
+
 df=pd.read_csv("customer_churn_dataset-testing-master.csv")
 # cleaning of data
 shape =df.shape
